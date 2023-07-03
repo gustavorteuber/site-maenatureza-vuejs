7291aa7d0e8092c4213b085500ffe85b74416a3d
gustavorteuber <gugareis135@gmail.com>
2023-07-03 19:46:48 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <navbar />
+  <carousel />
+  <cards />
+  <footerinfo />
+</template>
+<script>
+import navbar from "./components/Navbar.vue";
+import carousel from "./components/Carousel.vue";
+import cards from "./components/CardsJail.vue";
+import footerinfo from "./components/Footer.vue";
+
+export default {
+  components: { navbar, carousel, cards, footerinfo },
+};
+</script>
---
+++ src/components/CardsJail.vue
@@ -0,0 +1,68 @@
+<template>
+  <body>
+    <main
+      class="h-screen w-screen py-6 flex items-center justify-center flex-wrap"
+    >
+      <!--     primera card -->
+      <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="h-3/4 w-full">
+          <img
+            class="w-full h-full object-cover rounded-t"
+            src="https://images.pexels.com/photos/6157052/pexels-photo-6157052.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="piña"
+          />
+        </div>
+        <div class="w-full h-1/4 p-3">
+          <a href="#" class="hover:text-yellow-600 text-gray-700">
+            <span class="text-lg font-semibold uppercase tracking-wide"
+              >Pineapple</span
+            >
+          </a>
+          <p class="text-gray-600 text-sm leading-5 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+      <!--     segunda card -->
+      <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="h-3/4 w-full">
+          <img
+            class="w-full h-full object-cover rounded-t"
+            src="https://images.pexels.com/photos/5966630/pexels-photo-5966630.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="piña"
+          />
+        </div>
+        <div class="w-full h-1/4 p-3">
+          <a href="#" class="hover:text-yellow-600 text-gray-700">
+            <span class="text-lg font-semibold uppercase tracking-wide"
+              >Banana</span
+            >
+          </a>
+          <p class="text-gray-600 text-sm leading-5 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+      <!--     tercera card -->
+      <div class="bg-white w-72 h-96 shadow-md rounded m-3">
+        <div class="h-3/4 w-full">
+          <img
+            class="w-full h-full object-cover rounded-t"
+            src="https://images.pexels.com/photos/5217960/pexels-photo-5217960.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="piña"
+          />
+        </div>
+        <div class="w-full h-1/4 p-3">
+          <a href="#" class="hover:text-yellow-600 text-gray-700">
+            <span class="text-lg font-semibold uppercase tracking-wide"
+              >Mango</span
+            >
+          </a>
+          <p class="text-gray-600 text-sm leading-5 mt-1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+    </main>
+  </body>
+</template>
